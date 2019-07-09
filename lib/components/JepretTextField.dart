aa804436e61d23c430f22764d8bcaee80fde116d
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-07-09 09:37:59 +0700

    Finished registration UI

--- lib/components/JepretTextField.dart
+++ lib/components/JepretTextField.dart
@@ -5,16 +5,22 @@ class JepretTextField extends StatelessWidget {
   String hint;
   Widget icon;
   bool isPassword;
+  TextEditingController controller;
+  FocusNode focusNode;
 
   JepretTextField({
     this.hint,
     this.icon,
-    this.isPassword: false
+    this.isPassword: false,
+    this.controller,
+    this.focusNode
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
+      focusNode: focusNode,
       obscureText: isPassword,
       decoration: InputDecoration(
           prefixIcon: icon,
--- lib/routes/RegisterRoute.dart
+++ lib/routes/RegisterRoute.dart
@@ -3,8 +3,14 @@ import 'package:jepret/components/PrimaryButton.dart';
 import 'package:jepret/components/HeadingText.dart';
 import 'package:jepret/components/JepretTextField.dart';
 import 'package:jepret/constants/JepretColor.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 
 class RegisterRoute extends StatelessWidget {
+  FocusNode _focus_name = new FocusNode();
+  FocusNode _focus_email = new FocusNode();
+  FocusNode _focus_password = new FocusNode();
+  FocusNode _focus_repassword = new FocusNode();
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -12,22 +18,50 @@ class RegisterRoute extends StatelessWidget {
         elevation: 0,
         leading: IconButton(icon: Icon(Icons.arrow_back),color: JepretColor.PRIMARY_DARKER, onPressed: () {}),
       ),
-      body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-                  child: _renderHeading()
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-                  child: _renderTextFields()
-              ),
-              Spacer(),
-              _renderBottomButtons()
-            ],
-          )
+      body: FormKeyboardActions(
+        keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
+        actions: [
+          KeyboardAction(focusNode: _focus_name),
+          KeyboardAction(focusNode: _focus_email),
+          KeyboardAction(focusNode: _focus_password),
+          KeyboardAction(focusNode: _focus_repassword)
+        ],
+        child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints viewportConstraints) {
+              return SingleChildScrollView(
+                  child: ConstrainedBox(
+                      constraints: BoxConstraints(minHeight: viewportConstraints.maxHeight),
+                      child: IntrinsicHeight(
+                          child: SafeArea(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  Padding(
+                                      padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                                      child: _renderHeading()
+                                  ),
+                                  Padding(
+                                      padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                                      child: _renderTextFields()
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      mainAxisSize: MainAxisSize.max,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        _renderBottomButtons()
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )
+                          )
+                      )
+                  )
+              );
+            }
+        )
       )
     );
   }
@@ -52,13 +86,36 @@ class RegisterRoute extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          JepretTextField(hint: 'Nama lengkap', icon: Icon(Icons.person_outline)),
+          JepretTextField(
+            hint: 'Nama lengkap',
+            icon: Icon(Icons.person_outline),
+            focusNode: _focus_name,
+          ),
           Container(height: 12),
-          JepretTextField(hint: 'Alamat e-mail', icon: Icon(Icons.mail_outline)),
+          JepretTextField(
+            hint: 'Alamat e-mail',
+            icon: Icon(Icons.mail_outline),
+            focusNode: _focus_email,
+          ),
           Container(height: 12),
-          JepretTextField(hint: 'Kata sandi', icon: Icon(Icons.lock), isPassword: true,),
+          JepretTextField(
+            hint: 'Kata sandi',
+            icon: Icon(Icons.lock),
+            isPassword: true,
+            focusNode: _focus_password,
+          ),
           Container(height: 12),
-          JepretTextField(hint: 'Ulangi kata sandi', icon: Icon(Icons.lock_outline), isPassword: true,),
+          JepretTextField(
+            hint: 'Ulangi kata sandi',
+            icon: Icon(Icons.lock_outline),
+            isPassword: true,
+            focusNode: _focus_repassword,
+          ),
+          Container(height: 24),
+          Text(
+            "Dengan mendaftar, Anda dianggap menyetujui Syarat dan Ketentuan Penggunaan aplikasi Jepret",
+            style: TextStyle(color: Colors.black45, fontSize: 14),
+          )
         ],
       ),
     );
