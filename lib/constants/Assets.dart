39fc19c407577207eb32ed049ef25db413e719ea
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-07-11 11:25:36 +0700

    Added business profile offer

--- lib/constants/Assets.dart
+++ lib/constants/Assets.dart
@@ -1,4 +1,5 @@
 class Assets {
   static const String LOGO = "assets/img/logo.png";
   static const String ICON_QUEST = "assets/img/quest.png";
+  static const String ICON_SURPRISE_BOX = "assets/img/surprise-box.png";
 }
--- lib/pages/individual/IndividualProfilePage.dart
+++ lib/pages/individual/IndividualProfilePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jepret/constants/JepretColor.dart';
+import 'package:jepret/constants/Assets.dart';
 import 'package:jepret/components/JepretTextField.dart';
 import 'package:jepret/components/OutlinedPrimaryButton.dart';
 import 'package:keyboard_actions/keyboard_actions.dart';
@@ -10,10 +11,12 @@ class IndividualProfilePage extends StatefulWidget {
 
 class IndividualProfilePageState extends State<IndividualProfilePage> {
   FocusNode _focus_name = new FocusNode();
+  FocusNode _focus_email = new FocusNode();
   FocusNode _focus_mobile = new FocusNode();
   FocusNode _focus_nik = new FocusNode();
 
   TextEditingController _controller_name = new TextEditingController();
+  TextEditingController _controller_email = new TextEditingController();
   TextEditingController _controller_mobile = new TextEditingController();
   TextEditingController _controller_nik = new TextEditingController();
 
@@ -29,6 +32,7 @@ class IndividualProfilePageState extends State<IndividualProfilePage> {
                   keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
                   actions: [
                     KeyboardAction(focusNode: _focus_name),
+                    KeyboardAction(focusNode: _focus_email),
                     KeyboardAction(focusNode: _focus_mobile),
                     KeyboardAction(focusNode: _focus_nik),
                   ],
@@ -41,6 +45,7 @@ class IndividualProfilePageState extends State<IndividualProfilePage> {
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment.stretch,
                                       children: <Widget>[
+                                        _renderBusinessProfileRegistrationOffer(),
                                         _renderTextFields(),
                                         Expanded(
                                           child: Column(
@@ -83,7 +88,7 @@ class IndividualProfilePageState extends State<IndividualProfilePage> {
                   ),
                 ),
                 Text(
-                  "agungboba@gmail.com",
+                  "3271061010010001",
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
@@ -106,6 +111,15 @@ class IndividualProfilePageState extends State<IndividualProfilePage> {
               focusNode: _focus_name,
               controller: _controller_name,
               icon: Icon(Icons.person_outline),
+              hasFloatingPlaceholder: true,
+            ),
+            Container(height: 16),
+            JepretTextField(
+              label: "Alamat E-mail",
+              focusNode: _focus_email,
+              controller: _controller_email,
+              icon: Icon(Icons.mail_outline),
+              keyboardType: TextInputType.emailAddress,
               hasFloatingPlaceholder: true,
             ),
             Container(height: 16),
@@ -143,6 +157,59 @@ class IndividualProfilePageState extends State<IndividualProfilePage> {
             )
           ],
         )
+    );
+  }
+
+  Widget _renderBusinessProfileRegistrationOffer() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Material(
+        elevation: 1,
+        borderRadius: BorderRadius.circular(4),
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: JepretColor.PRIMARY_DARKER_10,
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: <Widget>[
+                  Image(image: AssetImage(Assets.ICON_SURPRISE_BOX), width: 72,),
+                  Container(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Buat profil usaha!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                        Text("Dapatkan banyak kemudahan dengan profil usaha", style: TextStyle(fontSize: 15, color: Colors.black54)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Divider(height: 1, color: Colors.black26),
+            Container(
+              color: JepretColor.PRIMARY_DARKER_10,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: FlatButton(
+                        child: Text(
+                          "Buat profil usaha",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: JepretColor.PRIMARY_DARKER
+                          )
+                        ),
+                        onPressed: () {},
+                      )
+                  )
+                ],
+              )
+            )
+          ],
+        )
+      )
     );
   }
 }
